3b668b4b849eb889181098f2f7d448a1a8369338
shubham devkar <shubhamdevkar@users.noreply.github.com>
2020-10-16 21:36:31 +0530

    Add files via upload

---
+++ cansat.ino
@@ -0,0 +1,103 @@
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+#define DHTTYPE DHT11   // DHT 11
+
+#define dht_dpin 4
+DHT dht(dht_dpin, DHTTYPE); 
+/*
+ * ESP8266 NodeMCU AJAX Demo
+ * Updates and Gets data from webpage without page refresh
+ * https://sdtechrobotics.com
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+#include "index.h" //Our HTML webpage contents with javascripts
+
+#define LED 2  //On board LED
+
+//SSID and Password of your WiFi router
+const char* ssid = "ssid";
+const char* password = "password";
+
+ESP8266WebServer server(80); //Server on port 80
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+
+void handleT() {
+  float t = dht.readTemperature();         
+  //int a = analogRead(A0);
+ Serial.print(t);
+ String temperature = String(t);
+ server.send(200, "text/plane", temperature); //Send ADC value only to client ajax request
+}
+void handleH() {
+  float h = dht.readHumidity();
+  Serial.println(h);
+ String humidity = String(h) ;
+ server.send(200, "text/plane", humidity);
+}
+
+void handleLED() {
+ String ledState = "OFF";
+ String t_state = server.arg("LEDstate"); //Refer  xhttp.open("GET", "setLED?LEDstate="+led, true);
+ Serial.println(t_state);
+ if(t_state == "1")
+ {
+  digitalWrite(LED,LOW); //LED ON
+  ledState = "ON"; //Feedback parameter
+ }
+ else
+ {
+  digitalWrite(LED,HIGH); //LED OFF
+  ledState = "OFF"; //Feedback parameter  
+ }
+ 
+ server.send(200, "text/plane", ledState); //Send web page
+}
+//==============================================================
+//                  SETUP
+//==============================================================
+void setup(void){
+  Serial.begin(115200);
+  dht.begin();
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  //Onboard LED port Direction output
+  pinMode(LED,OUTPUT); 
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+ 
+  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+  server.on("/setLED", handleLED);
+  server.on("/readT", handleT);
+  server.on("/readH", handleH);
+
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+//==============================================================
+//                     LOOP
+//==============================================================
+void loop(void){
+  server.handleClient();          //Handle client requests
+ }
